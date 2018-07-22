412eab0da7bc21e559896bba50cc5fb6a7ef7b80
Will Holdhusen <wholdhus@gmail.com>
2018-07-22 14:28:56 -0400

    Added original solution stuff back in

--- solve_rg_model.py
+++ solve_rg_model.py
@@ -58,8 +58,82 @@ def compute_particle_number(Delta, L, N, Z, g, Gamma):
     return n
 
 
-def compute_iom_energy(L, N, G, A, B, C, epsilon):
+def compute_iom_energy(L, N, G, model, epsilon):
     """Compute the exact energy using the integrals of motion.
+
+    Args:
+        L (int): system's size.
+        N (int): particle number.
+        G (float): interaction strength.
+        model (str): type of integrable model. Can be 'rational' or
+            'hyperbolic'.
+        epsilon (1darray of floats): epsilon values.
+
+    Returns:
+        E (float): ground state energy.
+        n (1darray of floats): occupation numbers.
+
+    """
+    # Determine the value of the constant Gamma.
+    if (model != 'rational') and (model != 'hyperbolic'):
+        raise ValueError('Model should be either rational or hyperbolic.')
+
+    Gamma = 0 if model == 'rational' else -1
+
+    # Compute Z matrix.
+    Z = np.zeros((L, L))
+    for i in range(L):
+        for j in range(i):  # j < i.
+            if model == 'hyperbolic':
+                Z[i, j] = (epsilon[i] + epsilon[j])/(epsilon[i]
+                                                     - epsilon[j])
+            elif model == 'rational':
+                Z[i, j] = 1/(epsilon[i] - epsilon[j])
+            Z[j, i] = -Z[i, j]
+
+    # Initial values for Delta with g small. The -2 values (initially
+    # occupied states) go where the epsilons are smallest.
+    delta = np.zeros(L, np.float64)
+    eps_min = np.argsort(epsilon)
+    delta[eps_min[:N]] = -2
+
+    # Points over which we will iterate until we reach G.
+    G_step = 0.0002
+    G_path = np.append(np.arange(0, G, G_step), G)
+    if model == 'rational':
+        # Lowercase g is the interaction strength of the equivalent
+        # integrable Hamiltonian.
+        g_path = -2*G_path
+        for g in g_path:
+            sol = root(delta_relations, delta, args=(L, N, Z, g, Gamma),
+                      method='lm')
+            delta = sol.x
+
+        # Eigenvalues of the IM.
+        ri = -1/2 - delta/2 + g/4*np.sum(Z, axis=1)
+        E = np.dot(epsilon, ri) + np.sum(epsilon)/2 + G*((N-L/2)**2 - N - L/4)
+        n = compute_particle_number(delta, L, N, Z, g, Gamma)
+
+    elif model == 'hyperbolic':
+        # Parametrize g to equivalent integrable Hamiltonian.
+        lambd = 1/(1 + G_path*(N - L/2 - 1))
+        g_path = -G_path*lambd
+        for g in g_path:
+            sol = root(delta_relations, delta, args=(L, N, Z, g, Gamma),
+                       method='lm')
+            delta = sol.x
+
+        # Eigenvalues of the IM.
+        ri = -1/2 - delta/2 + g/4*np.sum(Z, axis=1)
+        E = 1/lambd[-1]*np.dot(epsilon, ri) + np.sum(epsilon)*(1/2 - 3/4*G)
+        n = compute_particle_number(delta, L, N, Z, g, Gamma)
+
+    return E, n
+
+
+def compute_iom_energy_gen(L, N, G, A, B, C, epsilon):
+    """Compute the exact energy using the integrals of motion for
+	general-ish case
 
     Args:
         L (int): system's size.
