ac066c8cf792f38f0ea814110cf42f56024115a7
Will Holdhusen <wholdhus@gmail.com>
2018-07-24 17:04:01 -0400

    Confident this sort of works

--- plot_energy.py
+++ plot_energy.py
@@ -2,11 +2,11 @@ import numpy as np
 import matplotlib.pyplot as plt
 from solve_rg_model import compute_iom_energy_quad
 
-def plot_energies(A, B, C, steps=30):
-    Ls = [10, 20, 30]
-    glmax = .003
-    colors = ['m', 'c', 'r']
-    for j in range(3):
+def plot_energies(A, B, C, steps=20):
+    Ls = [10, 20, 50, 100]
+    gmax= 1
+    colors = ['m', 'c', 'r', 'g']
+    for j in range(4):
         L = Ls[j]
         print('Trying system size {}'.format(L))
         N = L//2
@@ -15,32 +15,37 @@ def plot_energies(A, B, C, steps=30):
 
         energy = np.array([0 for i in range(steps)])
         dEnergy = np.array([0 for i in range(steps)])
-        G = np.linspace(0.00001, glmax * L, steps)
+        G = np.linspace(0.00001, gmax/L, steps)
         for i in range(steps):
             energy[i], dEnergy[i] = compute_iom_energy_quad(
                     L, N, G[i], A, B, C, epsilon)
         plt.plot(
-            G/L, energy/L, label = "L = {}".format(L), color = colors[j])
+            G*L, energy/L, label = "L = {}".format(L), color = colors[j])
+
+def plot_energy(A, B, C, L, steps=11):
+    gmax = 1
+    N = L//4
+    eta = np.sin(np.linspace(1, 2*L-1, L)*np.pi/(4*L))
+    epsilon = eta**2
+    # epsilon = -2*np.cos(np.linspace(1, 2*L-1, L)*np.pi/(4*L))
+    energy = np.array([0 for i in range(steps)])
+    dEnergy = np.array([0 for i in range(steps)])
+    G = np.linspace(-gmax/L, gmax/L, steps)
+    for i in range(steps):
+        energy[i], dEnergy[i] = compute_iom_energy_quad(
+            L, N, G[i], A, B, C, epsilon)
+    plt.plot(G*L, energy/L, label = "a = {0}, b = {1}, c={2}".format(
+        A, B, C))
 
 
 if __name__ == '__main__':
+    plot_energy(1, 2, 1, 100)
+    plot_energy(1, 2, -1, 100)
+    plot_energy(0, 2, 0, 100)
+    plot_energy(1, 0, 0, 100)
+    plot_energy(2, 1, 2, 100)
+    plt.legend()
+    plt.xlabel('G')
+    plt.ylabel('E')
+    plt.show()
     
-    plt.subplot(3,1,1)
-    plot_energies(1, 4, 1)
-    plt.ylabel('E/L')
-    plt.xlabel('G/L')
-    plt.legend()
-
-    plt.subplot(3,1,2)
-    plot_energies(1, 4, -1)
-    plt.ylabel('E/L')
-    plt.xlabel('G/L')
-    plt.legend()
-
-    plt.subplot(3,1,3)
-    plot_energies(1, 4, 0)
-    plt.ylabel('E/L')
-    plt.xlabel('G/L')
-    plt.legend()
-
-    plt.show()    
