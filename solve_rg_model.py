aff8c45ffa9d9d24388e4d36619b26590b364c20
Will Holdhusen <wholdhus@gmail.com>
2019-01-30 09:15:52 -0500

    Code runs but gives bad results

--- solve_rg_model.py
+++ solve_rg_model.py
@@ -6,6 +6,23 @@ from numba import njit
 
 
 @njit()
+def complex_delta_relations(Double_Delta, L, N, Z, g, Gamma):
+    """Express the relations of the Delta parameters (Eqs 3 and 4).
+
+    The relations have the form of a vector whose entries are zeros.
+    """
+    rels = np.zeros(L+1, np.complex128)
+    Delta = Double_Delta[:L] + 1j * Double_Delta[L:]
+    # Eq 3.
+    rels[:L] = (-Delta**2 + g**2*N*(L-N)*Gamma - 2*Delta
+                + g*np.sum(Z, axis=1)*Delta - g*np.dot(Z, Delta))
+
+    # Eq 4.
+    rels[L] = np.sum(Delta) + 2*N
+
+    return np.concatenate((rels.real, rels.imag))
+
+
 def delta_relations(Delta, L, N, Z, g, Gamma):
     """Express the relations of the Delta parameters (Eqs 3 and 4).
 
@@ -72,6 +89,15 @@ def compute_particle_number(Delta, L, N, Z, g, Gamma):
     return n, A
 
 
+def make_Z(L, epsilon):
+    Z = np.zeros((L, L), np.complex128)
+    for i in range(L):
+        for j in range(i):  # j < i.
+            Z[i, j] = (epsilon[i] + epsilon[j])/(epsilon[i]-epsilon[j])
+            Z[j, i] = -Z[i, j]
+    return Z
+
+
 def compute_hyperbolic_energy(L, N, G, epsilon,
         g_step, holdover=0, taylor_expand=False,
         return_matrix=False):
@@ -89,17 +115,14 @@ def compute_hyperbolic_energy(L, N, G, epsilon,
 
     """
     Gamma = -1 # hyperbolic case
-
+    impart = np.cos(np.linspace(0, np.pi, L))
+    cepsilon = epsilon + 1j*impart
     # Compute Z matrix.
-    Z = np.zeros((L, L))
-    for i in range(L):
-        for j in range(i):  # j < i.
-            Z[i, j] = (epsilon[i] + epsilon[j])/(epsilon[i]-epsilon[j])
-            Z[j, i] = -Z[i, j]
+    Z = make_Z(L, cepsilon)
 
     # Initial values for Delta with g small. The -2 values (initially
     # occupied states) go where the epsilons are smallest.
-    delta = np.zeros(L, np.float64)
+    delta = np.zeros(L, np.complex128)
     eps_min = np.argsort(epsilon)
     delta[eps_min[:N]] = -2
 
@@ -116,29 +139,42 @@ def compute_hyperbolic_energy(L, N, G, epsilon,
     else:
         g_path = np.array([0])
     # Finding root while varying g, using prev. solution to start
+    double_delta = np.concatenate((delta.real, delta.imag))
     for g in g_path:
         if not np.isnan(g): # skipping steps where lambd broke
-            sol = root(delta_relations, delta, args=(L, N, Z, g, Gamma),
+            sol = root(complex_delta_relations, double_delta, args=(L, N, Z, g, Gamma),
                        method='lm')
         if np.isnan(g):
             print('Division by 0 problem at g={}'.format(g))
-        last = delta
-        delta = (1 - holdover) * sol.x + holdover * last
+        last = double_delta
+        double_delta = (1 - holdover) * sol.x + holdover * last
     g = g_final
     if holdover != 0:
-        sol = root(delta_relations, delta, args=(L, N, Z, g, Gamma),
+        sol = root(complex_delta_relations, double_delta, args=(L, N, Z, g, Gamma),
                 method = 'lm')
-        delta = sol.x
+        double_delta = sol.x
     # checking accuracy of solutions
-    dr = delta_relations(delta, L, N, Z, g, Gamma)
+    dr = complex_delta_relations(double_delta, L, N, Z, g, Gamma)
     if np.max(dr)> 10**-12:
         print('WARNING: At G= {} error is {}'.format(
                 G, np.max(dr)))
         success = False
     else: # at least reasonably accurate
         success = True
+    print('Before reducing impart, max diff is {}'.format(np.max(dr)))
+    imscale = 1 - np.linspace(0, 1, 100)
+    for i_s in imscale:
+        Z = make_Z(L, epsilon + i_s*1j*impart)
+        sol = root(complex_delta_relations, double_delta, args=(L, N, Z, g, Gamma),
+                   method='lm')
+        double_delta = sol.x
+    # Now throwing away the imaginary part (badbadbad)
+    delta = double_delta[:L]
+    # delta = double_delta[:L] + 1j*double_delta[L:]
+    # print(delta)
 
     # Now forming eigenvalues of IM and observables
+    Z = make_Z(L, epsilon)
     ri = -1/2 - delta/2 + g/4*np.sum(Z, axis=1)
     E = 1/lambd*np.dot(epsilon, ri) + np.sum(epsilon)*(1/2 - 3/4*G)
     n, A = compute_particle_number(delta, L, N, Z, g, Gamma)
@@ -257,3 +293,17 @@ def rgk_spectrum(L, t1, t2):
     k = np.linspace(0, 1, L)*np.pi
     epsilon = -0.5 * t1 * (np.cos(k) - 1) - 0.5 * t2 * (np.cos(2*k) -1)
     return k, epsilon
+
+if __name__ == '__main__':
+    import matplotlib.pyplot as plt
+    L = 50
+    N = 30
+    k, epsilon = rgk_spectrum(L, 1, 0)
+    Gc = 1./(L-2*N+1)
+    Gs = np.linspace(1.5*Gc, 0, 15)
+    energies = np.zeros(15)
+    for i, G in enumerate(Gs):
+        energies[i], _, _, _ = compute_hyperbolic_energy(L, N, G, epsilon, 0.01)
+    plt.scatter(Gs, energies)
+    plt.axvline(Gc)
+    plt.show()
