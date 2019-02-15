0791715a7658566e332387defcaf698d3ef93124
Will Holdhusen <wholdhus@gmail.com>
2019-02-15 14:55:09 -0500

    more tweaks to make this nicer on karst

--- rgk.py
+++ rgk.py
@@ -10,29 +10,29 @@ def do_rgk():
     N = 512
     k, rgke = rgk_spectrum(L, 1, 0, peri=False)
     epsilon = rgke
-    G = 3.0/L
     if len(sys.argv) > 1:
         g_step = float(sys.argv[1])/L
     else:
         g_step = 0.1/L
     print('Parameters: {} {} {}'.format(L, N, g_step))
-    l = L/2
-    n = N/2
+    l = int(L/2)
+    n = int(N/2)
+    G = 3.0/l
     energies, nsk, deltas, Gs, Z = compute_hyperbolic_energy(l, n, G, epsilon, g_step)
     energies = 8*energies - 2*(N-L/2)
 
     df = pd.DataFrame({'G': Gs, 'E': energies})
-    df.to_csv('results/rgk_energies_{}.csv'.format(g_step))
+    df.to_csv('results/rgk_energies_{}_{}.csv'.format(G, np.round(g_step, 2)))
     Ns = pd.DataFrame({'k': k, 'epsilon': epsilon}, index = k)
     Deltas = pd.DataFrame({'k': k, 'epsilon': epsilon}, index = k)
     for i, G in enumerate(Gs):
         Ns['G'] = nsk[i]
         Deltas['G'] = deltas[i]
-    Ns.to_csv('results/rgk_ns_{}.csv'.format(g_step))
-    Deltas.to_csv('results/rgk_deltas_{}.csv'.format(g_step))
+    Ns.to_csv('results/rgk_ns_{}_{}.csv'.format(G, np.round(g_step, 2)))
+    Deltas.to_csv('results/rgk_deltas_{}_{}.csv'.format(G, np.round(g_step, 2)))
 
 if __name__ == '__main__':
     start = time.time()
-    test_rgk()
+    do_rgk()
     finish = time.time()
     print('Seconds elapsed: {}'.format(finish-start))
